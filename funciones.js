0300e7e66c6ca8e02fc9d712e930db5331d1eac8
Chaste <b.chiastellino@gmail.com>
2023-08-22 23:57:08 -0300

    add funciones

--- funciones.js
+++ funciones.js
@@ -1,28 +1,34 @@
-    function calcularEdad (edad){
-        if(edad >= 18){
-            alert("La edad ingresada es mayor de edad");
-        }else{
-            alert("La edad ingresada es menor de edad");
-        }
-    }
+function calcularEdad(edad) {
+  if (edad >= 18) {
+    alert("La edad ingresada es mayor de edad");
+  } else {
+    alert("La edad ingresada es menor de edad");
+  }
+}
 
-    
-    
-    function notificarNota (nota){
-        if(nota >= 0 && nota <=3){
-            alert(`Su nota ${nota} es insuficiente`);
-        }
-        else if (nota >= 3 && nota <= 5){
-            alert(`Su nota ${nota} es insuficiente`);
-        } else if (nota == 5 || nota == 6){
-            alert('Su nota ${nota} es suficiente');
-        }  else if (nota >= 7 && nota <= 9){
-            alert('Su nota ${nota} es notable');
-        } else if (nota == 9 || nota == 10){
-            alert('Su ${nota} es sobresaliente');
-        }else{
-            alert("El digito ingresado no es valido");
-        }
-    }
+function notificarNota(nota) {
 
-    
+    console.log(nota);
+    console.log(typeof(nota));
+    console.log(isNaN(nota));
+
+  if (!isNaN(nota)) {
+    if (nota >= 0 && nota <= 3) {
+      alert(`Su nota ${nota} es muy deficiente`);
+    } else if (nota >= 3 && nota <= 5) {
+      alert(`Su nota ${nota} es insuficiente`);
+    } else if (nota == 5 || nota == 6) {
+      alert(`Su nota ${nota} es suficiente`);
+    } else if (nota == 6 || nota == 7) {
+      alert(`Su nota ${nota} es bien`);
+    } else if (nota >= 7 && nota <= 9) {
+      alert(`Su nota ${nota} es notable`);
+    } else if (nota == 9 || nota == 10) {
+      alert(`Su nota ${nota} es sobresaliente`);
+    }
+  } else {
+    alert("El digito ingresado no es valido");
+  }
+}
+
+
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+const Persona = requiere(./);
 let opcion = parseFloat(prompt("Ingrese numero ejercicio"));
-
 
 switch (opcion) {
   case 1:
@@ -8,14 +8,36 @@ switch (opcion) {
   case 2:
     segundoEjercicio();
     break;
+    case 3:
+      tercerEjercicio();
+    break;
 }
 
 function primerEjercicio() {
-    let edad = parseFloat(prompt("Ingrese su edad"));
-    calcularEdad(edad);
-  }
+  let edad = parseFloat(prompt("Ingrese su edad"));
+  calcularEdad(edad);
+}
+
+function segundoEjercicio() {
+  let nota = parseFloat(prompt("Ingrese nota"));
+  notificarNota(nota);
+}
+
+function tercerEjercicio (){
+
+  let listaPeronas = new Array(Persona);
+  let persona1 = new Persona("Brian", 24);
+
+  listaPeronas.push(persona1);
+
+
+  console.log(listaPeronas[0]);
+ 
+
   
-  function segundoEjercicio() {
-    let nota = parseFloat(prompt("Ingrese nota"));
-    notificarNota(nota);
-  }
+
+
+
+}
+
+
