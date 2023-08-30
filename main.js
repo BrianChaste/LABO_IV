45300502afbea83f30186ecd3306d42055ad1970
Chaste <b.chiastellino@gmail.com>
2023-08-29 23:00:59 -0300

    add html-js-csss

--- main.js
+++ main.js
@@ -1,49 +1,79 @@
-let opcion = parseFloat(prompt("Ingrese una opcion"));
+function miFuncion() {
+   
+  let numero = document.getElementById("numero").value;
 
-switch (opcion) {
-  case 1:
-    primerEjercicio();
-    break;
-  case 2:
-    segundoEjercicio();
-    break;
-  case 3:
-    tercerEjercicio();
-    break;
-  case 4:
-    cuaertoEjercicio();
-    break;
-  case 5:
-    ejercicioCinco();
-    break;
-  case 6:
-    ejercicioSeis();
-    break;
-  case 7:
-    ejercicioSiete();
-    break;
-  case 8:
-    ejerciciOcho();
-    break;
-  case 9:
-    ejercicioNueve();
-    break;
-  case 10:
-    ejerciciDiez();
-    break;
-  case 11:
-    ejercicioOnce();
-    break;
+  numero = parseFloat(numero);
+
+  switch (numero) {
+    case 1:
+      primerEjercicio();
+      break;
+    case 2:
+      segundoEjercicio();
+      break;
+    case 3:
+      tercerEjercicio();
+      break;
+    case 4:
+      cuaertoEjercicio();
+      break;
+    case 5:
+      ejercicioCinco();
+      break;
+    case 6:
+      ejercicioSeis();
+      break;
+    case 7:
+      ejercicioSiete();
+      break;
+    case 8:
+      ejerciciOcho();
+      break;
+    case 9:
+      ejercicioNueve();
+      break;
+    case 10:
+      ejerciciDiez();
+      break;
+    case 11:
+      ejercicioOnce();
+      break;
+    default:
+      alert("Incorrecto");
+      break;
+  }
 }
 
 function primerEjercicio() {
   let edad = parseFloat(prompt("Ingrese su edad"));
-  calcularEdad(edad);
+
+  if (edad >= 18) {
+    alert("La edad ingresada es mayor de edad");
+  } else {
+    alert("La edad ingresada es menor de edad");
+  }
 }
 
 function segundoEjercicio() {
   let nota = parseFloat(prompt("Ingrese nota"));
-  notificarNota(nota);
+
+  if (!isNaN(nota)) {
+    if (nota >= 0 && nota <= 3) {
+      alert(`Su nota ${nota} es muy deficiente`);
+    } else if (nota >= 3 && nota <= 5) {
+      alert(`Su nota ${nota} es insuficiente`);
+    } else if (nota == 5 || nota == 6) {
+      alert(`Su nota ${nota} es suficiente`);
+    } else if (nota == 6 || nota == 7) {
+      alert(`Su nota ${nota} es bien`);
+    } else if (nota >= 7 && nota <= 9) {
+      alert(`Su nota ${nota} es notable`);
+    } else if (nota == 9 || nota == 10) {
+      alert(`Su nota ${nota} es sobresaliente`);
+    }
+  } else {
+    alert("El digito ingresado no es valido");
+  }
 }
 
 function cuaertoEjercicio() {
@@ -157,7 +187,7 @@ function ejercicioOnce() {
   const radio = parseFloat(prompt("Ingrese el radio del circulo"));
   const PI = 3.1416;
   let area;
-  area = PI * (radio ** 2);
+  area = PI * radio ** 2;
 
   /////////////////////////////////////////
 
