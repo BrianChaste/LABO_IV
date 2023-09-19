d477a9953af913ef0046386d3b603b905f366771
Brian Chaste <b.chiastellino@gmail.com>
2023-09-19 18:32:58 -0300

    add css tp3

--- TPN3/main.js
+++ TPN3/main.js
@@ -3,11 +3,18 @@ import { getPersonajesAPI } from "./Conexion-API.js";
 const imagenData = document.getElementById("imagen");
 const boton = document.getElementById("boton");
 const botonJugar = document.getElementById("jugar-boton");
+const game = document.getElementById("game");
+
+function mostrarJuego () {
+    game.style.display = 'block';
+}
 
 
 async function mostrarPersonaje(){
     const personajes = await getPersonajesAPI();
     let personajeAleatorio = personajes[Math.floor(Math.random() * personajes.length)];
+
+    mostrarJuego();
     
     imagenData.setAttribute("src", personajeAleatorio.Imagen);
     imagenData.setAttribute("alt",personajeAleatorio.Nombre);
@@ -16,6 +23,8 @@ async function mostrarPersonaje(){
     //!console.log(imagenData.getAttribute("alt"));
 
 }
+
+
 
 botonJugar.addEventListener("click", mostrarPersonaje);
 
